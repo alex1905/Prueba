6018dd7b7a0f5baf245b8683a47830f12bfd881c
alex1905 <alexvas_91@yahoo.es>
2014-04-11 20:57:19 -0600

    CuentaVip
    Ahora son 3 cuentas en ves de 2

---
+++ Cuentas/Cuentas/CuentaVip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuentas
+{
+    class CuentaVip:CuentaBanco
+    {
+
+         public CuentaVip(string idCuenta, string nombreUsuario, float balance, float interes, string tipoCuenta)
+            : base(idCuenta, nombreUsuario, balance, interes, tipoCuenta)
+        {
+            this.tipoCuenta = "Vip";
+            this.interes = 0.8f;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+"";
+        }
+    }
+}
